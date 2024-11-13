997985a1d41d02b5d378c1645ee7f9d4574dd1f2
TriksOwO <Emailforsiteslog@gmail.com>
2024-11-13 17:59:51 +0100

    API  key check
    added function to check if API key loaded properly

--- zadanie.py
+++ zadanie.py
@@ -2,15 +2,16 @@ import openai
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path="D:\\zadanie\\secrets.env")
 
 file_path = "Zadanie dla JJunior AI Developera - tresc artykulu.txt"
 print(f"aktualny katalog roboczy: {os.getcwd()}")
 print(f"ścieżka pliku: {os.path.abspath(file_path)}")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+
 
 if os.path.exists(file_path):
-    print("file found")
+    print(openai.api_key)
 else:
     print("file not found")
 
@@ -18,11 +19,14 @@ else:
 def read_article(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
-        if text:
-            print(f"file text: {text}")
-        else:
-            print("file is empty")
-        
+        if not text:
+            print(f"file {file_path} is empty")
+            return  None
         return text
     
 article_text = read_article(file_path)
+if api_key:
+    openai.api_key = api_key
+    print(f"API key: {openai.api_key}")
+else:
+    print("API key not found")
