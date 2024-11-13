8e4f5bb5c886e93f96b83a4651d5c60db657c108
TriksOwO <Emailforsiteslog@gmail.com>
2024-11-13 16:23:47 +0100

    print
    file print if not empty

--- zadanie.py
+++ zadanie.py
@@ -3,7 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-file_path = "D:\"Zadanie dla JJunior AI Developera - tresc artykulu.txt"
+
+file_path = "Zadanie dla JJunior AI Developera - tresc artykulu.txt"
+print(f"aktualny katalog roboczy: {os.getcwd()}")
+print(f"ścieżka pliku: {os.path.abspath(file_path)}")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 if os.path.exists(file_path):
@@ -14,4 +17,12 @@ else:
 
 def read_article(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+        text = file.read()
+        if text:
+            print(f"file text: {text}")
+        else:
+            print("file is empty")
+        
+        return text
+    
+article_text = read_article(file_path)
